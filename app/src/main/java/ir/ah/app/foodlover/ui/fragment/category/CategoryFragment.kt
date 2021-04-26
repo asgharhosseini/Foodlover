0ac924d50dd72b27b040a44b8f5ceb272beda8a9
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-26 17:01:05 +0430

    create CategoryFragment.kt for get data and show in ui

--- app/src/main/java/ir/ah/app/foodlover/ui/fragment/category/CategoryFragment.kt
+++ app/src/main/java/ir/ah/app/foodlover/ui/fragment/category/CategoryFragment.kt
@@ -1,6 +1,86 @@
 package ir.ah.app.foodlover.ui.fragment.category
 
-import androidx.fragment.app.Fragment
+import android.os.*
+import android.util.*
+import android.view.*
+import androidx.lifecycle.*
+import androidx.navigation.fragment.*
+import androidx.recyclerview.widget.*
+import com.google.android.material.snackbar.*
+import dagger.hilt.android.*
 import ir.ah.app.foodlover.R
+import ir.ah.app.foodlover.base.*
+import ir.ah.app.foodlover.other.*
+import ir.ah.app.foodlover.ui.adapter.*
+import kotlinx.android.synthetic.main.fragemnt_category.*
+import javax.inject.*
 
-class CategoryFragment : Fragment(R.layout.fragemnt_category)
+@AndroidEntryPoint
+class CategoryFragment : BaseFragment<CategoryViewModel>(R.layout.fragemnt_category, CategoryViewModel::class) {
+    @Inject
+    lateinit var categoryAdapter: CategoryAdapter
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        subscribeToObservers()
+        setUpRecyclerViews()
+        onClick()
+
+    }
+
+    private fun onClick() {
+
+        categoryAdapter.setOnItemClickListener {
+            findNavController().navigate(CategoryFragmentDirections.actionCategoryFragmentToAllRestaurantFragment())
+        }
+        iv_fragmentCategory_backBottom.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun subscribeToObservers() {
+
+        viewModel.category.observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Resource.Success -> {
+
+                    result.data?.let {
+                        categoryAdapter.differ.submitList(it)
+
+
+                    }
+                }
+                is Resource.Error -> {
+
+                    result.message?.let { message ->
+                        Log.e(Constance.TAG, message)
+                        Snackbar.make(
+                                requireView(),
+                                "Error: $message ${result.code}",
+                                Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
+                is Resource.Loading -> Unit
+            }
+        })
+
+
+    }
+
+    private fun setUpRecyclerViews() {
+        rv_fragmentCategory.apply {
+            adapter = categoryAdapter
+            layoutManager =
+                    GridLayoutManager(requireContext(), 3)
+        }
+
+    }
+
+
+}
