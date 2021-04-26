add7119ea308c68df1197876e91238b03dd5d276
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-26 14:59:50 +0430

    create FakeData.kt for const fake data

---
+++ app/src/main/java/ir/ah/app/foodlover/data/remot/repositoeies/FakeData.kt
@@ -0,0 +1,409 @@
+package ir.ah.app.foodlover.data.remot.repositoeies
+
+import ir.ah.app.foodlover.*
+
+object FakeData {
+
+    val restaurantName = arrayListOf<String>(
+            "سرآشپز",
+            "مهتاب",
+            "یاس",
+            "خورشید",
+            "تخت جمشید",
+            "سیب",
+            "حسن رشتی",
+            "اکبر جوجه ",
+            "پدیده",
+            "سراب"
+    )
+    val restaurantCategoryName = arrayListOf<String>(
+            "بین الملی",
+            "کبابی",
+            "جکرکی",
+            "کره ای",
+            "سنتی",
+            "فست فود",
+            "آشی",
+            "ایتالیایی ",
+            "کافه",
+            "جوجه ای"
+    )
+    val restaurantImageName = arrayListOf<String>(
+            "https://blog.okala.com/wp-content/uploads/2019/07/%D9%82%D9%88%D8%B1%D9%85%D9%87-%D8%B3%D8%A8%D8%B2%DB%8C.jpg",
+            "https://i.pinimg.com/originals/51/a8/b8/51a8b825c90acb6860a6e3056e6466f5.jpg",
+            "https://i.pinimg.com/originals/e7/d7/38/e7d7383501f66883806e722ff4d3081f.jpg",
+            "https://snapp.market/blog/wp-content/uploads/2020/04/kebab3.jpg",
+            "https://hotelyar.com/mag/uploads/blog/GOHARSHAD-MOSQUE/shandiz/kandovan/Atigh-mosque/famouris%20house/chehel-sotoun/kish%20time%20travel/kish%20retaurant/zosk-village/coffee/NewFolder/kariz-e-kish/%D8%B3%D8%A7%D9%86%D8%A7/Bank-password/%D8%B1%D8%B2%D8%B1%D9%88%20%D9%87%D8%AA%D9%84/%D8%AE%D8%A7%D9%86%D9%87%20%D8%A8%D9%87%D9%86%D8%A7%D9%85/%D8%AE%D8%A7%D9%86%D9%87%20%D8%B3%DB%8C%D9%85%DB%8C%D9%86%20%D8%AF%D8%A7%D9%86%D8%B4%D9%88%D8%B1/%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86%20%D9%87%D8%A7%DB%8C%20%D9%85%D8%B4%D9%87%D8%AF/%D9%85%D9%82%D8%A7%DB%8C%D8%B3%D9%87%20%D9%82%D8%B4%D9%85%20%D9%88%20%DA%A9%DB%8C%D8%B4/%D8%A8%D8%B3%D8%AA%D9%86%DB%8C/%D8%BA%D8%B0%D8%A7%DB%8C%20%D8%B3%D9%86%D8%AA%DB%8C%20%D9%85%D8%B4%D9%87%D8%AF/Mashahad-traditional-food-HOTELYAR-.jpg",
+            " https://blog.okala.com/wp-content/uploads/2020/10/lunch.jpg",
+            "https://img9.irna.ir/d/r2/2019/11/12/3/156756061.jpg",
+            "https://damdary.com/assets/uploads/news/1FEC2624-0756-4A43-8FF0-C2BA4B92CD19--0.jpg ",
+            "https://files.virgool.io/upload/users/16237/posts/jl5rz1pqxbqd/5sudpkj4wlha.jpeg",
+            "https://jamejamonline.ir/files/fa/news/1399/9/15/181915_716.jpg"
+    )
+
+    val restaurantAppetizerName = arrayListOf<String>(
+            "توپ پنیری",
+            "زیتون پرورده",
+            "سالاد آووکادو با پنیر فتا",
+            "سالاد اسفناج با نارنگی",
+            "سالاد تن ماهی با سبزیجات",
+            "سالاد تن ماهی با لوبیا",
+            "سالاد زیتون",
+            "سالاد عدس",
+            "سالاد لبو با پنیر",
+            "سالاد لبو و گشنیز",
+            "سیب زمینی له شده با پنیر",
+            "گل کلم سرخ شده در فر",
+            "گواکامولی",
+            "ناچوز فلفل دلمهای و مرغ",
+            "نان پرشده با پنیر و گشنیز",
+            "نان پنیری با زیتون",
+            "نان سیر",
+            "نان سیر"
+    )
+    val restaurantAppetizerImage = arrayListOf<String>(
+            "https://www.cookingandcooking.com/cook-db/pictures/Cheese-Ball.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Zeytoon-Parvardeh.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Cucumber-Avocado-Salad-Feta.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Spinach-Salad-with-Mandarin-Orange.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Tuna-Vegetable-Salad.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Tuna-Black-Eyed-Peas-Salad.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Olive-Salad.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Simple-Lentil-Salad.jpg ",
+            "https://www.cookingandcooking.com/cook-db/pictures/Stacked-Beet-Salad-with-Feta-Cheese.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Beet-and-Coriander-Salad.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Smashed-Potatoes-with-Cheese.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Spicy-Roasted-Cauliflower.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Guacamole-Avocado-Dip.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Bell-Pepper-Chicken-Nachos.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Stuffed-Cheesy-Bread.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Olive-Chesse-Bread.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Garlic-Bread.jpg",
+            "https://www.cookingandcooking.com/cook-db/pictures/Garlic-Bread.jpg"
+    )
+    val restaurantAppetizerPrice = arrayListOf<String>(
+            "10000",
+            "5000",
+            "12000",
+            "14000",
+            "17000",
+            "14000",
+            "13000",
+            "17000",
+            "16000",
+            "12000",
+            "17000",
+            "14000",
+            "13000",
+            "17000",
+            "13000",
+            "17000",
+            "16000",
+            "12000",
+            "12000"
+    )
+
+    val restaurantMainCourseName = arrayListOf<String>(
+            "مگشت پلو",
+            "باچانگای گوشت",
+            "کوکوی اشپل",
+            "کباب یونانی",
+            "سوفله بادمجان",
+            "ته چین اسفناج",
+            "مرغ وحشی",
+            "خوراک ماهی ",
+            "خوراک گل کلم",
+            "قورمه سبزی تهرانی",
+            "هش براون",
+            "تره اسفناج",
+            "خاویار بادمجان",
+            "قیمه یزدی",
+            "کوبیده مرغ تابه ای",
+            "پوکورا بوشهری",
+            "شله بریان (اصفهان)",
+            "دال عدس (غذای تند جنوبی)",
+            "بادمجان پارمزان"
+
+    )
+    val restaurantMainCourseImage = arrayListOf<String>(
+            "https://irancook.ir/wp-content/uploads/2021/02/%D9%85%DA%AF%D8%B4%D8%AA-%D9%BE%D9%84%D9%88.jpg",
+            "https://irancook.ir/wp-content/uploads/2021/03/%D8%A8%D8%A7%DA%86%D8%A7%D9%86%DA%AF%D8%A7%DB%8C-%DA%AF%D9%88%D8%B4%D8%AA-min.jpg",
+            "https://irancook.ir/wp-content/uploads/2021/02/%DA%A9%D9%88%DA%A9%D9%88%DB%8C-%D8%A7%D8%B4%D9%BE%D9%84-min.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/11/%DA%A9%D8%A8%D8%A7%D8%A8-%DB%8C%D9%88%D9%86%D8%A7%D9%86%DB%8C-min.jpg",
+            "https://irancook.ir/wp-content/uploads/2021/03/%D8%B3%D9%88%D9%81%D9%84%D9%87-%D8%A8%D8%A7%D8%AF%D9%85%D8%AC%D8%A7%D9%86-min.jpeg",
+            "https://irancook.ir/wp-content/uploads/2021/02/%D8%AA%D9%87-%DA%86%DB%8C%D9%86-%D8%A7%D8%B3%D9%81%D9%86%D8%A7%D8%AC-min.jpg",
+            "https://irancook.ir/wp-content/uploads/2021/01/%D8%B7%D8%B1%D8%B2-%D8%AA%D9%87%DB%8C%D9%87-%D9%85%D8%B1%D8%BA-%D9%88%D8%AD%D8%B4%DB%8C.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/10/%D8%AE%D9%88%D8%B1%D8%A7%DA%A9-%D9%85%D8%A7%D9%87%DB%8C-%D8%B3%D9%81%DB%8C%D8%AF-min.jpg ",
+            "https://irancook.ir/wp-content/uploads/2020/10/%D8%B7%D8%B1%D8%B2-%D8%AA%D9%87%DB%8C%D9%87-%D8%AE%D9%88%D8%B1%D8%A7%DA%A9-%DA%AF%D9%84-%DA%A9%D9%84%D9%85.jpeg",
+            "https://irancook.ir/wp-content/uploads/2020/10/%D8%AE%D9%88%D8%B1%D8%B4%D8%AA-%D9%82%D9%88%D8%B1%D9%85%D9%87-%D8%B3%D8%A8%D8%B2%DB%8C.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/10/%D9%87%D8%B4-%D8%A8%D8%B1%D8%A7%D9%88%D9%86-%D8%A8%D8%A7-%D9%86%DB%8C%D9%85%D8%B1%D9%88.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/10/%D8%AA%D8%B1%D9%87-%D8%A7%D8%B3%D9%81%D9%86%D8%A7%D8%AC-min.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/10/%D8%AE%D8%A7%D9%88%DB%8C%D8%A7%D8%B1-%D8%A8%D8%A7%D8%AF%D9%85%D8%AC%D8%A7%D9%86.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/10/%D9%82%DB%8C%D9%85%D9%87-%DB%8C%D8%B2%D8%AF%DB%8C-min.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/10/%DA%A9%D9%88%D8%A8%DB%8C%D8%AF%D9%87-%D9%85%D8%B1%D8%BA-1.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/09/%D9%BE%D9%88%DA%A9%D9%88%D8%B1%D8%A7-%D8%A8%D9%88%D8%B4%D9%87%D8%B1%DB%8C-.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/08/%D8%B4%D9%84%D9%87-%D8%A8%D8%B1%DB%8C%D8%A7%D9%86-%D8%A7%D8%B5%D9%81%D9%87%D8%A7%D9%86-.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/08/%D8%AF%D8%A7%D9%84-%D8%B9%D8%AF%D8%B3-min.jpg",
+            "https://irancook.ir/wp-content/uploads/2020/08/%DA%AF%D8%B1%D8%A7%D8%AA%D9%86-%D8%A8%D8%A7%D8%AF%D9%85%D8%AC%D8%A7%D9%86.jpg"
+    )
+    val restaurantMainCoursePrice = arrayListOf<String>(
+            "32000",
+            "33000",
+            "34000",
+            "36000",
+            "35000",
+            "38000",
+            "35000",
+            "34000",
+            "36000",
+            "34000",
+            "36000",
+            "32000",
+            "37000",
+            "37000",
+            "38000",
+            "36000",
+            "34000",
+            "31000",
+            "33000"
+    )
+
+    val restaurantDessertName = arrayListOf<String>(
+            "دسر ساده زعفرانی",
+            "تارت بستنی",
+            "پودینگ تخم شربتی",
+            "ژله رنگین کمان",
+            "ژله میوه",
+            "شیر برنج",
+            "فرنی",
+            "شیرینی شکلاتی آلمانی",
+            "سوهان عسلی کنجدی",
+            "دسر بیسکوییتی بدون فر",
+            "دسر هندوانه",
+            "ایرمیک توت فرنگی",
+            "دسر خرمالو",
+            " پودینگ کاراملی",
+            "کاسترد",
+            "پاناکوتای وانیلی",
+            "پودینگ شکلاتی",
+            "بیسکویت و ژله",
+            "لوکومادس"
+    )
+    val restaurantDessertImage = arrayListOf<String>(
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69214_720.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69195_156.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69174_632.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69169_431.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69191_759.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69213_468.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69172_244.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69176_873.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69198_194.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69170_774.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69223_412.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69215_227.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69222_654.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69218_138.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69221_872.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69217_391.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69219_703.jpg",
+            "https://tabnakjavan.com/files/fa/news/1398/8/17/69220_898.jfif",
+            "https://tabnakjavan.com/files/fa/news/1399/5/19/148815_475.jpg"
+    )
+    val restaurantDessertPrice = arrayListOf<String>(
+            "12000",
+            "13000",
+            "14000",
+            "16000",
+            "15000",
+            "18000",
+            "15000",
+            "14000",
+            "16000",
+            "14000",
+            "16000",
+            "12000",
+            "17000",
+            "17000",
+            "18000",
+            "16000",
+            "14000",
+            "11000",
+            "13000"
+    )
+
+    val restaurantBeveragesName = arrayListOf<String>(
+            "شرک، غول سبز مهربان",
+            "باب اسفنجی",
+            "کاپرینا",
+            "مانیومنت",
+            "چاپلین",
+            "شیک وانیل و گردو",
+            "شیک کره بادوم زمینی",
+            "شیک موز و شکلات",
+            "شیک شکلات",
+            "آب پرتقال طبیعی",
+            "شیراز",
+            "مانا",
+            "شادیاخ",
+            "موهیتو",
+            "اودیسه",
+            "لیموناد",
+            "چای سرد",
+            "کلاسیک کافی فلوت",
+            "آیس موکا"
+    )
+    val restaurantBeveragesImage = arrayListOf<String>(
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/1e72ba7051755822d162a7592537f4c0.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/86d1485b597fad5757dd5eb7d7a79398.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/865006d71303e633e847f3296b7ca34f.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/96680f420283a5b95b2b283238d62c2c.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/ac78ef6b8f13e7a8b21ab33bd604a2e9.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/d2889d1d6f8f43554d5ec21c4b2e914e.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/446379d5844bb560a8c56697e6b7522d.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/e79b450e4439c15952b168eda1008312.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/e79b450e4439c15952b168eda1008312.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/7619202a9fc3986bfbd09ca4d9064b64.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/ce173b14e81903d6e808f3205ca25d66.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/7822041915efb917d92751ec937204c1.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/bdfc0fbff9b2ee3a8cf7ced5f1a6c91d.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/865006d71303e633e847f3296b7ca34f.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/12f74c260ad89ac0c7d9113960e4007f.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/1bd1ac2aa598d290272aaa60aa209d0b.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/1d1e3dd07f146a5adc77e102895c3c74.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/907bcad00f8b72762c46e48ca5fbb5ae.jpg",
+            "http://cinemacafe.ir/cache/Nima/Food%20and%20Drinks/c86e8a1938c08eac813ae953d37450b5.jpg"
+    )
+    val restaurantBeveragesPrice = arrayListOf<String>(
+            "12000",
+            "13000",
+            "14000",
+            "16000",
+            "15000",
+            "18000",
+            "15000",
+            "14000",
+            "16000",
+            "14000",
+            "16000",
+            "12000",
+            "17000",
+            "17000",
+            "18000",
+            "16000",
+            "14000",
+            "11000",
+            "13000"
+    )
+
+    val categoryName = arrayListOf<String>(
+            "غذا",
+            "نوشیدنی",
+            "گیاهی",
+            "برگر",
+            "کیک",
+            "دسر",
+            "پیتزا",
+            "نودل",
+            "شیک ",
+            "مرغ",
+            "مکزیکی",
+            "قهوه"
+    )
+    val categoryImageName = arrayListOf<Int>(
+            R.drawable.food,
+            R.drawable.drink,
+            R.drawable.vege,
+            R.drawable.burger,
+            R.drawable.coke,
+            R.drawable.dessert,
+            R.drawable.pizza,
+            R.drawable.noodles,
+            R.drawable.milktea,
+            R.drawable.chicken,
+            R.drawable.mexicanfood,
+            R.drawable.coffee
+    )
+    val categoryColor = arrayListOf<String>(
+            "#FB3061",
+            "#5653D4",
+            "#08A791",
+            "#FAA33F",
+            "#5653D4",
+            "#FB3061",
+            "#E5454C",
+            "#08A791",
+            "#5653D4",
+            "#FAA33F",
+            "#FB3061",
+            "#B6644F"
+    )
+
+
+    val bannerName = arrayListOf<String>(
+            "سرآشپز",
+            "مهتاب",
+            "یاس",
+            "خورشید",
+            "تخت جمشید",
+            "سیب",
+            "حسن رشتی",
+            "اکبر جوجه ",
+            "پدیده",
+            "سراب"
+    )
+    val bannerSubtitle = arrayListOf<String>(
+            "کرج",
+            "تهران",
+            "هشتگرد",
+            "عظیمیه",
+            "محمد شهر",
+            "فردیس",
+            "حصارک",
+            "باغستان",
+            "گوهردشت ",
+            "جهانشهر",
+            "طالقانی",
+            "رجایی شهر"
+    )
+    val bannerImageName = arrayListOf<String>(
+            "https://cdn.takhfifan.com/images/1.0?id=/149457/6/_/6_357_16.jpg",
+            "https://cdn.takhfifan.com/images/1.0?id=/149333/_/2/_2__500_7.jpg",
+            "https://cdn.takhfifan.com/images/1.0?id=/200410/2/_/2_955_25.jpg",
+            "https://cdn.takhfifan.com/images/1.0?id=/200345/2/_/2_956_33.jpg",
+            "https://cdn.takhfifan.com/images/1.0?id=/200408/1/_/1_838_8_2_1_11_1_1.jpg",
+            "https://cdn.takhfifan.com/images/1.0?id=/200177/i/m/image_413_1_34_1_69_1_9_1_34_1_29_1_16.jpg",
+            "https://cdn.takhfifan.com/images/1.0?id=/199171/7/_/7_311_3.jpg",
+            "https://cdn.takhfifan.com/images/1.0?id=/155471/_/1/_13__64_6.jpg",
+            "https://cdn.takhfifan.com/images/1.0?id=/192338/p/9/p9_1_2.jpg",
+            "https://cdn.takhfifan.com/images/1.0?id=/172519/_/2/_2__485_32_1_2_1_1_1_1.jpg"
+    )
+    val bannerColor = arrayListOf<String>(
+            "#FB3061",
+            "#5653D4",
+            "#08A791",
+            "#FAA33F",
+            "#5653D4",
+            "#FB3061",
+            "#E5454C",
+            "#08A791",
+            "#5653D4",
+            "#FAA33F",
+            "#FB3061",
+            "#B6644F"
+    )
+    val bannerCode = arrayListOf<String>(
+            "FB3061",
+            "5653D4",
+            "08A791",
+            "FAA33F",
+            "5653D4",
+            "FB3061",
+            "E5454C",
+            "08A791",
+            "5653D4",
+            "FAA33F",
+            "FB3061",
+            "B6644F"
+    )
+}
