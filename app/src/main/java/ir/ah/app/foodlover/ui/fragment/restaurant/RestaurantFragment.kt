a245948239dd99f0efb238c38bde6f33140e8a09
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-26 22:10:32 +0430

    add TabPagerAdapter in RestaurantFragment.kt

--- app/src/main/java/ir/ah/app/foodlover/ui/fragment/restaurant/RestaurantFragment.kt
+++ app/src/main/java/ir/ah/app/foodlover/ui/fragment/restaurant/RestaurantFragment.kt
@@ -1,6 +1,45 @@
 package ir.ah.app.foodlover.ui.fragment.restaurant
 
-import androidx.fragment.app.Fragment
-import ir.ah.app.foodlover.R
+import android.os.*
+import android.view.*
+import androidx.fragment.app.*
+import com.google.android.material.tabs.*
+import ir.ah.app.foodlover.*
+import ir.ah.app.foodlover.ui.adapter.*
+import kotlinx.android.synthetic.main.fragment_restaurant.*
 
-class RestaurantFragment : Fragment(R.layout.fragemnt_restaurant)
+class RestaurantFragment : Fragment(R.layout.fragment_restaurant) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+
+    }
+
+    private fun initView() {
+        configureTabLayout()
+    }
+
+    private fun configureTabLayout() {
+
+        tabLayout.addTab(tabLayout.newTab().setText("پیش غذا"))
+        tabLayout.addTab(tabLayout.newTab().setText("غدا اصلی"))
+        tabLayout.addTab(tabLayout.newTab().setText("دسر"))
+        tabLayout.addTab(tabLayout.newTab().setText("نوشیدنی"))
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter = TabPagerAdapter(parentFragmentManager, tabLayout.tabCount)
+        vp_fragmentRestaurant.adapter = adapter
+        vp_fragmentRestaurant.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                vp_fragmentRestaurant.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+
+        })
+    }
+}
