fdb426cdfec6e49a6886d03521271a9af8a04634
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-26 15:04:31 +0430

    create SearchAdapter.kt for generate data in recycler

---
+++ app/src/main/java/ir/ah/app/foodlover/ui/adapter/SearchAdapter.kt
@@ -0,0 +1,47 @@
+package ir.ah.app.foodlover.ui.adapter
+
+
+import android.view.*
+import com.bumptech.glide.*
+import ir.ah.app.foodlover.R
+import ir.ah.app.foodlover.data.model.restaurant.*
+import kotlinx.android.synthetic.main.item_search.view.*
+import javax.inject.*
+
+class SearchAdapter @Inject constructor(
+        private val glide: RequestManager
+) : BaseAdapter<Restaurant>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return BaseViewHolder(
+                LayoutInflater.from(parent.context)
+                        .inflate(
+                                R.layout.item_search,
+                                parent, false
+                        )
+        )
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        val restaurant = differ.currentList[position]
+        holder.itemView.apply {
+            txt_itemPopular_title.text = restaurant.title
+            txt_itemPopular_category.text = restaurant.categories
+            chip_popular_distance.text = restaurant.distance
+            chip_popular_distanceTime.text = restaurant.timeDistance
+            setOnClickListener {
+                onItemClickListener?.let { click ->
+                    click(restaurant)
+                }
+            }
+            glide.load(restaurant.image).into(img_itemPopular)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+
+}
