eab5a12bb3a967ad4bf5d995490aa854e4c1bc88
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-26 17:02:33 +0430

    create DiscountFragment.kt for get data and show in ui

--- app/src/main/java/ir/ah/app/foodlover/ui/fragment/discount/DiscountFragment.kt
+++ app/src/main/java/ir/ah/app/foodlover/ui/fragment/discount/DiscountFragment.kt
@@ -1,6 +1,82 @@
 package ir.ah.app.foodlover.ui.fragment.discount
 
-import androidx.fragment.app.Fragment
+import android.os.*
+import android.util.*
+import android.view.*
+import androidx.lifecycle.*
+import androidx.navigation.fragment.*
+import androidx.recyclerview.widget.*
+import com.google.android.material.snackbar.*
+import dagger.hilt.android.*
 import ir.ah.app.foodlover.R
+import ir.ah.app.foodlover.base.*
+import ir.ah.app.foodlover.other.*
+import ir.ah.app.foodlover.ui.adapter.*
+import kotlinx.android.synthetic.main.fragemnt_discount.*
+import javax.inject.*
 
-class DiscountFragment : Fragment(R.layout.fragemnt_discount)
+@AndroidEntryPoint
+class DiscountFragment : BaseFragment<DiscountViewModel>(R.layout.fragemnt_discount, DiscountViewModel::class) {
+
+    @Inject
+    lateinit var bannerAdapter: BannerAdapter
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        subscribeToObservers()
+        setUpRecyclerViews()
+        onClick()
+
+    }
+
+    private fun onClick() {
+        iv_fragmentDiscount_backBottom.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+    }
+
+    private fun subscribeToObservers() {
+
+
+        viewModel.banner.observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Resource.Success -> {
+
+                    result.data?.let {
+                        bannerAdapter.differ.submitList(it)
+                        bannerAdapter.differ.submitList(it)
+                    }
+                }
+                is Resource.Error -> {
+                    result.message?.let { message ->
+                        Log.e(Constance.TAG, message)
+                        Snackbar.make(
+                                requireView(),
+                                "Error: $message ${result.code}",
+                                Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
+                is Resource.Loading -> Unit
+            }
+        })
+
+    }
+
+    private fun setUpRecyclerViews() {
+
+        rv_fragmentDiscount.apply {
+            adapter = bannerAdapter
+            layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
+    }
+
+
+}
