2761963bdbd95c564811f05f2501ed04aaa1d6ec
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-27 15:08:55 +0430

    get data form repository and pass to view

---
+++ app/src/main/java/ir/ah/app/foodlover/ui/fragment/restaurant/RestaurantViewModel.kt
@@ -0,0 +1,48 @@
+package ir.ah.app.foodlover.ui.fragment.restaurant
+
+import androidx.lifecycle.*
+import dagger.hilt.android.lifecycle.*
+import ir.ah.app.foodlover.base.*
+import ir.ah.app.foodlover.data.model.restaurant.*
+import ir.ah.app.foodlover.data.remot.repositoeies.restaurant.*
+import ir.ah.app.foodlover.other.*
+import kotlinx.coroutines.*
+import java.io.*
+import javax.inject.*
+
+@HiltViewModel
+class RestaurantViewModel @Inject constructor(private val repository: RestaurantRepositoryImpl) :
+        BaseViewModel() {
+
+    private val _restaurant = MutableLiveData<Resource<Restaurant>>()
+    val restaurant: LiveData<Resource<Restaurant>> = _restaurant
+
+
+    init {
+        getAllRestaurant()
+    }
+
+    fun getAllRestaurant() = viewModelScope.launch {
+        _restaurant.postValue(Resource.Loading())
+        try {
+            val response = repository.getRestaurant()
+            if (response != null) {
+                _restaurant.postValue(Resource.Success(response))
+            }
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> _restaurant.postValue(
+                        Resource.Error(
+                                "Network Failure",
+                                null,
+                                404
+                        )
+                )
+                else -> _restaurant.postValue(Resource.Error("Conversion Error", null, 404))
+            }
+
+        }
+    }
+
+
+}
