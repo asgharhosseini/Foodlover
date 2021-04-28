b6aa9222197cc3d4232548b8ca7c3eab8843197c
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-28 17:03:42 +0430

    create FeaturedFragment.kt and PopularFragment.kt and TrendingFragment.kt and NewestFragment.kt for item tab in AllRestaurantFragment.kt

---
+++ app/src/main/java/ir/ah/app/foodlover/ui/fragment/allrestaurant/trending/TrendingFragment.kt
@@ -0,0 +1,88 @@
+package ir.ah.app.foodlover.ui.fragment.allrestaurant.trending
+
+import android.os.*
+import android.util.*
+import android.view.*
+import androidx.lifecycle.*
+import androidx.navigation.fragment.*
+import androidx.recyclerview.widget.*
+import com.google.android.material.snackbar.*
+import dagger.hilt.android.*
+import ir.ah.app.foodlover.R
+import ir.ah.app.foodlover.base.*
+import ir.ah.app.foodlover.other.*
+import ir.ah.app.foodlover.ui.adapter.*
+import ir.ah.app.foodlover.ui.fragment.allrestaurant.*
+import kotlinx.android.synthetic.main.fragment_trending.*
+
+
+import javax.inject.*
+
+@AndroidEntryPoint
+class TrendingFragment : BaseFragment<AllRestaurantViewModel>(R.layout.fragment_trending, AllRestaurantViewModel::class) {
+
+    @Inject
+    lateinit var allRestaurantAdapter: AllRestaurantAdapter
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        subscribeToObservers()
+        setUpRecyclerViews()
+        onClick()
+
+    }
+
+    private fun onClick() {
+
+        allRestaurantAdapter.setOnItemClickListener {
+            findNavController().navigate(AllRestaurantFragmentDirections.actionAllRestaurantFragmentToRestaurantFragment())
+        }
+
+    }
+
+    private fun subscribeToObservers() {
+
+        viewModel.restaurant.observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Resource.Success -> {
+
+                    result.data?.let {
+                        allRestaurantAdapter.differ.submitList(it)
+                        allRestaurantAdapter.differ.submitList(it)
+
+                    }
+                }
+                is Resource.Error -> {
+
+                    result.message?.let { message ->
+                        Log.e(Constance.TAG, message)
+                        Snackbar.make(
+                                requireView(),
+                                "Error: $message ${result.code}",
+                                Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
+                is Resource.Loading -> Unit
+            }
+        })
+
+
+    }
+
+    private fun setUpRecyclerViews() {
+        rv_fragmentTrending.apply {
+            adapter = allRestaurantAdapter
+            layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
+
+    }
+
+
+}
