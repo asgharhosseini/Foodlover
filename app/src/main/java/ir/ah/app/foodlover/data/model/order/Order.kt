3706a90ae2cee5aaa2033b28f8bf4c0dfd4a8e75
asghar hosseini <asgharhosseini.v@gmail.com>
2021-05-01 17:28:09 +0430

    add item and annotation for create table in data base

--- app/src/main/java/ir/ah/app/foodlover/data/model/order/Order.kt
+++ app/src/main/java/ir/ah/app/foodlover/data/model/order/Order.kt
@@ -2,18 +2,24 @@ package ir.ah.app.foodlover.data.model.order
 
 import android.annotation.SuppressLint
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @SuppressLint("ParcelCreator")
 @Parcelize
+@Entity(tableName = "order_table")
 data class Order(
         @SerializedName("id")
-        var id: Int,
+        @PrimaryKey(autoGenerate = true)
+        var id: Int? = null,
         @SerializedName("title")
         var title: String,
         @SerializedName("image")
         var image: String,
         @SerializedName("price")
-        var price: String
+        var price: String,
+        @SerializedName("count")
+        var count: Int = 1
 ) : Parcelable
