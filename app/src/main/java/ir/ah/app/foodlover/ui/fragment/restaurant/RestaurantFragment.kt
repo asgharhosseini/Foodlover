4b7239259618bf3aa05f5b4b735f03b2b91ecc26
asghar hosseini <asgharhosseini.v@gmail.com>
2021-05-01 17:36:09 +0430

    get order count and navigate to AllOrderFragment.kt

--- app/src/main/java/ir/ah/app/foodlover/ui/fragment/restaurant/RestaurantFragment.kt
+++ app/src/main/java/ir/ah/app/foodlover/ui/fragment/restaurant/RestaurantFragment.kt
@@ -1,24 +1,27 @@
 package ir.ah.app.foodlover.ui.fragment.restaurant
 
-import android.os.*
-import android.util.*
-import android.view.*
-import androidx.lifecycle.*
-import androidx.navigation.fragment.*
-import androidx.recyclerview.widget.*
-import com.bumptech.glide.*
-import com.google.android.material.appbar.*
-import com.google.android.material.snackbar.*
-import com.google.android.material.tabs.*
-import dagger.hilt.android.*
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.RequestManager
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import dagger.hilt.android.AndroidEntryPoint
 import ir.ah.app.foodlover.R
-import ir.ah.app.foodlover.base.*
-import ir.ah.app.foodlover.data.model.order.*
-import ir.ah.app.foodlover.other.*
-import ir.ah.app.foodlover.ui.adapter.*
-import ir.ah.app.foodlover.ui.dialog.*
+import ir.ah.app.foodlover.base.BaseFragment
+import ir.ah.app.foodlover.data.model.order.Order
+import ir.ah.app.foodlover.other.Constance
+import ir.ah.app.foodlover.other.NumberHelper
+import ir.ah.app.foodlover.other.Resource
+import ir.ah.app.foodlover.ui.adapter.MainCourseAdapter
+import ir.ah.app.foodlover.ui.adapter.TabPagerMenuAdapter
+import ir.ah.app.foodlover.ui.dialog.OrderDialog
 import kotlinx.android.synthetic.main.fragment_restaurant.*
-import javax.inject.*
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RestaurantFragment : BaseFragment<RestaurantViewModel>(R.layout.fragment_restaurant, RestaurantViewModel::class) {
@@ -82,11 +85,14 @@ class RestaurantFragment : BaseFragment<RestaurantViewModel>(R.layout.fragment_r
 
         dailyAdapter.setOnItemClickListener {
             val dialog = OrderDialog(Order(it.id, it.title, it.image, it.price),
-                    onClicked = {
-                        Snackbar.make(requireView(), this.title, Snackbar.LENGTH_LONG).show()
+                onClicked = {
+                    Snackbar.make(requireView(), this.title, Snackbar.LENGTH_LONG).show()
 
-                    })
+                })
             dialog.show(parentFragmentManager, null)
+        }
+        btn_allOrder.setOnClickListener {
+            findNavController().navigate(RestaurantFragmentDirections.actionRestaurantFragmentToAllOrderFragment())
         }
 
     }
@@ -113,14 +119,18 @@ class RestaurantFragment : BaseFragment<RestaurantViewModel>(R.layout.fragment_r
                     result.message?.let { message ->
                         Log.e(Constance.TAG, message)
                         Snackbar.make(
-                                requireView(),
-                                "Error: $message ${result.code}",
-                                Snackbar.LENGTH_LONG
+                            requireView(),
+                            "Error: $message ${result.code}",
+                            Snackbar.LENGTH_LONG
                         ).show()
                     }
                 }
                 is Resource.Loading -> Unit
             }
+        })
+
+        viewModel.totalCount.observe(viewLifecycleOwner, Observer {
+            btn_allOrder.text = "سفارشات شما (${NumberHelper.EnglishToPersian(it.toString())})"
         })
 
 
--- app/src/main/java/ir/ah/app/foodlover/ui/fragment/restaurant/RestaurantViewModel.kt
+++ app/src/main/java/ir/ah/app/foodlover/ui/fragment/restaurant/RestaurantViewModel.kt
@@ -1,14 +1,17 @@
 package ir.ah.app.foodlover.ui.fragment.restaurant
 
-import androidx.lifecycle.*
-import dagger.hilt.android.lifecycle.*
-import ir.ah.app.foodlover.base.*
-import ir.ah.app.foodlover.data.model.restaurant.*
-import ir.ah.app.foodlover.data.remot.repositoeies.restaurant.*
-import ir.ah.app.foodlover.other.*
-import kotlinx.coroutines.*
-import java.io.*
-import javax.inject.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.ah.app.foodlover.base.BaseViewModel
+import ir.ah.app.foodlover.data.model.order.Order
+import ir.ah.app.foodlover.data.model.restaurant.Restaurant
+import ir.ah.app.foodlover.data.remot.repositoeies.restaurant.RestaurantRepositoryImpl
+import ir.ah.app.foodlover.other.Resource
+import kotlinx.coroutines.launch
+import java.io.IOException
+import javax.inject.Inject
 
 @HiltViewModel
 class RestaurantViewModel @Inject constructor(private val repository: RestaurantRepositoryImpl) :
@@ -17,6 +20,9 @@ class RestaurantViewModel @Inject constructor(private val repository: Restaurant
     private val _restaurant = MutableLiveData<Resource<Restaurant>>()
     val restaurant: LiveData<Resource<Restaurant>> = _restaurant
 
+    val totalSum = repository.observeTotalSum()
+    val totalCount = repository.observeTotalCount()
+    val allOrderItems = repository.observeAllOrderItems()
 
     init {
         getAllRestaurant()
@@ -32,11 +38,11 @@ class RestaurantViewModel @Inject constructor(private val repository: Restaurant
         } catch (t: Throwable) {
             when (t) {
                 is IOException -> _restaurant.postValue(
-                        Resource.Error(
-                                "Network Failure",
-                                null,
-                                404
-                        )
+                    Resource.Error(
+                        "Network Failure",
+                        null,
+                        404
+                    )
                 )
                 else -> _restaurant.postValue(Resource.Error("Conversion Error", null, 404))
             }
@@ -44,5 +50,12 @@ class RestaurantViewModel @Inject constructor(private val repository: Restaurant
         }
     }
 
+    fun deleteOrderItem(order: Order) = viewModelScope.launch {
+        repository.deleteShoppingItem(order)
+    }
+
+    fun insertOrderItemIntoDb(order: Order) = viewModelScope.launch {
+        repository.insertShoppingItem(order)
+    }
 
 }
