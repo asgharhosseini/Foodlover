0de1eb73b99d37be6bfd649dc12d0ee7abffb891
asghar hosseini <asgharhosseini.v@gmail.com>
2021-05-01 17:32:54 +0430

    get instance database and implement create OrderRepository.kt and get data form database

--- app/src/main/java/ir/ah/app/foodlover/data/remot/repositoeies/restaurant/RestaurantRepositoryImpl.kt
+++ app/src/main/java/ir/ah/app/foodlover/data/remot/repositoeies/restaurant/RestaurantRepositoryImpl.kt
@@ -1,13 +1,40 @@
 package ir.ah.app.foodlover.data.remot.repositoeies.restaurant
 
-import ir.ah.app.foodlover.data.model.restaurant.*
-import ir.ah.app.foodlover.data.remot.repositoeies.*
-import javax.inject.*
+import androidx.lifecycle.LiveData
+import ir.ah.app.foodlover.data.loacal.OrderDao
+import ir.ah.app.foodlover.data.model.order.Order
+import ir.ah.app.foodlover.data.model.restaurant.Restaurant
+import ir.ah.app.foodlover.data.remot.repositoeies.OrderRepository
+import ir.ah.app.foodlover.data.remot.repositoeies.RestaurantRepository
+import javax.inject.Inject
 
-class RestaurantRepositoryImpl @Inject constructor(private val restaurantRepositoryFake: RestaurantRepositoryFake) :
-        RestaurantRepository {
+class RestaurantRepositoryImpl @Inject constructor(
+    private val restaurantRepositoryFake: RestaurantRepositoryFake,
+    private val database: OrderDao
+) :
+    RestaurantRepository, OrderRepository {
     override suspend fun getRestaurant(): Restaurant {
         return restaurantRepositoryFake.getRestaurant()
+    }
+
+    override suspend fun insertShoppingItem(order: Order) {
+        database.insertShoppingItem(order)
+    }
+
+    override suspend fun deleteShoppingItem(order: Order) {
+        database.deleteShoppingItem(order)
+    }
+
+    override fun observeAllOrderItems(): LiveData<List<Order>> {
+        return database.observeAllOrderItems()
+    }
+
+    override fun observeTotalCount(): LiveData<Int> {
+        return database.observeTotalCount()
+    }
+
+    override fun observeTotalSum(): LiveData<Float> {
+        return database.observeTotalSum()
     }
 
 
