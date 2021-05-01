9b492a70a17fc8bbdbe78e8f0c9e243ebebdcfce
asghar hosseini <asgharhosseini.v@gmail.com>
2021-05-01 17:29:50 +0430

    provide database dependency

--- app/src/main/java/ir/ah/app/foodlover/di/AppModule.kt
+++ app/src/main/java/ir/ah/app/foodlover/di/AppModule.kt
@@ -1,17 +1,23 @@
 package ir.ah.app.foodlover.di
 
-import android.content.*
-import com.bumptech.glide.*
-import com.bumptech.glide.load.engine.*
-import com.bumptech.glide.load.resource.bitmap.*
-import com.bumptech.glide.request.*
-import dagger.*
-import dagger.hilt.*
-import dagger.hilt.android.qualifiers.*
-import dagger.hilt.components.*
+import android.content.Context
+import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import ir.ah.app.foodlover.R
+import ir.ah.app.foodlover.data.loacal.OrderDao
+import ir.ah.app.foodlover.data.loacal.OrderDatabase
 import ir.ah.app.foodlover.ui.adapter.*
-import javax.inject.*
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -54,6 +60,11 @@ object AppModule {
     fun provideBeveragesAdapter(glide: RequestManager) = BeveragesAdapter(glide)
 
     @Singleton
+
+    @Provides
+    fun provideOrderAdapter(glide: RequestManager) = OrderAdapter(glide)
+
+    @Singleton
     @Provides
     fun provideCategoryAdapter() = CategoryAdapter()
 
@@ -61,12 +72,29 @@ object AppModule {
     @Singleton
     @Provides
     fun provideGlideInstance(
-            @ApplicationContext context: Context
+        @ApplicationContext context: Context
     ) = Glide.with(context).setDefaultRequestOptions(
         RequestOptions().placeholder(R.drawable.logobase)
             .error(R.drawable.logobase)
             .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(16)))
             .diskCacheStrategy(DiskCacheStrategy.DATA)
     )
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext appContext: Context): OrderDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            OrderDatabase::class.java,
+            "order_db"
+        ).allowMainThreadQueries()
+            .build()
+    }
+
+    @Provides
+    fun provideOrderDao(orderDatabase: OrderDatabase): OrderDao {
+        return orderDatabase.orderDao()
+    }
+
 
 }
