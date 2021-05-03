be5ff65ea633b102b3406b9a99bf074eb68dfd15
asghar hosseini <asgharhosseini.v@gmail.com>
2021-05-03 16:39:44 +0430

    check user auth and navigate required

--- app/src/main/java/ir/ah/app/foodlover/ui/fragment/Splash/SplashFragment.kt
+++ app/src/main/java/ir/ah/app/foodlover/ui/fragment/Splash/SplashFragment.kt
@@ -1,48 +1,41 @@
 package ir.ah.app.foodlover.ui.fragment.Splash
 
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
-import dagger.hilt.android.AndroidEntryPoint
+import android.os.*
+import android.view.*
+import androidx.fragment.app.*
+import androidx.navigation.fragment.*
+import com.google.android.material.snackbar.*
+import dagger.hilt.android.*
 import ir.ah.app.foodlover.R
+import ir.ah.app.foodlover.other.*
+import javax.inject.*
+
 @AndroidEntryPoint
-class SplashFragment:Fragment(R.layout.fragment_spalsh) {
+class SplashFragment : Fragment(R.layout.fragment_spalsh) {
+
+    @Inject
+    lateinit var userInfoManager: UserInfoManager
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val mainThreadHandler= Handler(Looper.getMainLooper())
+        val mainThreadHandler = Handler(Looper.getMainLooper())
         mainThreadHandler.postDelayed({
             try {
-//                checkSituations()
-//                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToIntroitFragment())
-                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
-//
+                checkAuth()
             } catch (e: Exception) {
                 Snackbar.make(requireView(), e.message.toString(), Snackbar.LENGTH_LONG).show()
             }
         }, 2000)
 
     }
-//    fun checkSituations() {
-////        val networkAvailable = NetworkUtils.isNetworkAvailable(requireActivity())
-//        if (!networkAvailable) {
-////            Log.i(Constance.TAG,"No Connection")
-//            // Do somethings
-//        } else {
-//            Log.i(Constance.TAG,"Connected to internet ")
-//            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
-//
-//            /*if (TextUtils.isEmpty(TokenContainer.token)) {
-//                DebugHelper.info("Token Invalid")
-//                findNavController().navigate(R.id.action_splashFragment_to_registerFragment)
-//            } else {
-//                DebugHelper.info("Everything is ok, Go to dashboard")
-//                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
-//            }*/
-//        }
-//    }
+
+    private fun checkAuth() {
+        val token = userInfoManager.getToken()
+        if (!token.isNullOrEmpty()) {
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
+        } else {
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToIntroitFragment())
+        }
+    }
 }
