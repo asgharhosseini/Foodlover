b47c1ed91fd33e2538e02250997f3119cbc46bb1
asghar hosseini <asgharhosseini.v@gmail.com>
2021-05-10 12:18:19 +0430

    add const value in Constance.kt

--- app/src/main/java/ir/ah/app/foodlover/other/Constance.kt
+++ app/src/main/java/ir/ah/app/foodlover/other/Constance.kt
@@ -3,5 +3,7 @@ package ir.ah.app.foodlover.other
 object Constance {
     const val TAG = "debug"
     const val SHARED_PREFERENCES_NAME = "userInfo"
+    const val REQUEST_CODE_LOCATION_PERMISSION = 0
+
 
 }
