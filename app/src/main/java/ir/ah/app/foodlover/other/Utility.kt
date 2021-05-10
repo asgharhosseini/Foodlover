195d74e8b8a9473e689219f6f0939401996d938b
asghar hosseini <asgharhosseini.v@gmail.com>
2021-05-10 12:17:39 +0430

    create Utility.kt for check  hasLocationPermissions

---
+++ app/src/main/java/ir/ah/app/foodlover/other/Utility.kt
@@ -0,0 +1,23 @@
+package ir.ah.app.foodlover.other
+
+import android.*
+import android.content.*
+import android.os.*
+import pub.devrel.easypermissions.*
+
+object Utility {
+    fun hasLocationPermissions(context: Context) =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+        }
+}
