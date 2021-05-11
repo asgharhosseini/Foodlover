fd49b11a407d19426221eee7ee0052ab95237c63
asghar hosseini <asgharhosseini.v@gmail.com>
2021-05-11 13:48:26 +0430

    get user location and restaurant for show in map

--- app/src/main/java/ir/ah/app/foodlover/ui/fragment/allorder/AllOrderFragment.kt
+++ app/src/main/java/ir/ah/app/foodlover/ui/fragment/allorder/AllOrderFragment.kt
@@ -1,11 +1,13 @@
 package ir.ah.app.foodlover.ui.fragment.allorder
 
+import android.*
 import android.os.*
 import android.view.*
 import androidx.lifecycle.*
 import androidx.navigation.fragment.*
 import androidx.recyclerview.widget.*
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.*
 import dagger.hilt.android.*
 import ir.ah.app.foodlover.R
 import ir.ah.app.foodlover.base.*
@@ -14,27 +16,41 @@ import ir.ah.app.foodlover.other.*
 import ir.ah.app.foodlover.ui.adapter.*
 import ir.ah.app.foodlover.ui.fragment.restaurant.*
 import kotlinx.android.synthetic.main.fragment_all_order.*
+import pub.devrel.easypermissions.*
 import javax.inject.*
 
 @AndroidEntryPoint
 class AllOrderFragment :
     BaseFragment<RestaurantViewModel>(R.layout.fragment_all_order, RestaurantViewModel::class),
-    OrderAdapter.OrderEventListener {
+    OrderAdapter.OrderEventListener, OnMapReadyCallback, EasyPermissions.PermissionCallbacks {
 
     @Inject
     lateinit var orderAdapter: OrderAdapter
     var map: GoogleMap? = null
+    private val arg by navArgs<AllOrderFragmentArgs>()
 
+    @Inject
+    lateinit var userInfoManager: UserInfoManager
 
+    private var restaurantLatLng: LatLng? = null
+    private var userLatLng: LatLng? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mapView.onCreate(savedInstanceState)
-
+        requestPermissions()
         initView()
     }
 
     private fun initView() {
+        restaurantLatLng = LatLng(arg.latitude!!.toDouble(), arg.longitude!!.toDouble())
+        userLatLng = LatLng(
+            userInfoManager.getLatitude()!!.toDouble(),
+            userInfoManager.getLongitude()!!.toDouble()
+        )
+
         txt_fragmentOrder_gift.text = NumberHelper.EnglishToPersian("0") + "%"
+        txt_allOrderFragment_userLocationName.text = userInfoManager.getLocationName()
+
         toolbar.navigationIcon = resources.getDrawable(R.drawable.ic_baseline_arrow_back_ios_new_24)
         toolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
@@ -43,6 +59,13 @@ class AllOrderFragment :
         subscribeToObservers()
         setUpRecyclerViews()
         onClick()
+        setupMap()
+
+    }
+
+    private fun setupMap() {
+
+        mapView.getMapAsync(this)
 
     }
 
@@ -113,6 +136,59 @@ class AllOrderFragment :
 
     override fun onDelete(order: Order, position: Int) {
         viewModel.deleteOrderItem(order)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map!!.addMarker(MarkerOptions().position(userLatLng).title("آدرس شما"))
+        map!!.addMarker(MarkerOptions().position(restaurantLatLng).title("محل رستوران"))
+        map?.animateCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 14f))
+
+
+    }
+
+    private fun requestPermissions() {
+        if (Utility.hasLocationPermissions(requireContext())) {
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                Constance.REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                Constance.REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        setupMap()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 
 
